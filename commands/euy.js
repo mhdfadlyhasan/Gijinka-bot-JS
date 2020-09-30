cca05877aab437cf65270995b54a38a6493d9ccb
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2020-09-30 15:33:08 +0700

    Add Euy Functionality

--- commands/euy.js
+++ commands/euy.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'euy',
     execute(msg,args){
-        msg.channel.send('aawokowiakoawoiawoawki',
+        msg.channel.send('ayeuna poe rebo euy',
             {
-                // file: 'https://i.kym-cdn.com/photos/images/newsfeed/001/897/645/8a2.jpg'
+                file: './Asset/rebo.mp4'
             }
         )
     }

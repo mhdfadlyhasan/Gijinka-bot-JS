3b1fdf0eb284599cb7044613db50bf52156a4958
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2020-09-30 15:33:32 +0700

    Update classFunc, add Wednesday scchedule

--- classFunc.js
+++ classFunc.js
@@ -4,16 +4,26 @@ classes = []
 
 function assignKelas(client){
     //senin
-    classes.push(new Kelas(client, '759774771847757834', 1, '06:50', 'Jaringan Nirkabel'))//jarnil
-    classes.push(new Kelas(client, '759774530444591126', 1, '09:50', 'Realita Virtual dan Augmentasi'))//rva
-    classes.push(new Kelas(client, '759774622534336562', 1, '09:50', 'Pemrograman Perangkat Bergerak'))//PPB
-    classes.push(new Kelas(client, '759774588140912661', 1, '12:50', 'Teknopreneurship'))//teknopreneur
+    classes.push(new Kelas(client, '759774771847757834', 1, '07:00', 'Jaringan Nirkabel'))//jarnil
+    classes.push(new Kelas(client, '759774530444591126', 1, '10:00', 'Realita Virtual dan Augmentasi'))//rva
+    classes.push(new Kelas(client, '759774622534336562', 1, '10:00', 'Pemrograman Perangkat Bergerak'))//PPB
+    classes.push(new Kelas(client, '759774588140912661', 1, '13:00', 'Teknopreneurship'))//teknopreneur
     
     //selasa
-    classes.push(new Kelas(client, '760318003543408690', 2, '09:50', 'Evolusi Perangkat Lunak'))//EPL
-    classes.push(new Kelas(client, '760318042566688798', 2, '12:50', 'Komputasi Bergerak'))//Komber
-    classes.push(new Kelas(client, '760318072719933481', 2, '12:50', 'Sistem Temu Kembali Informasi'))//STKI
-    classes.push(new Kelas(client, '760318108849930280', 2, '15:50', 'Visi Komputer'))//Viskom
+    classes.push(new Kelas(client, '760318003543408690', 2, '10:00', 'Evolusi Perangkat Lunak'))//EPL
+    classes.push(new Kelas(client, '760318042566688798', 2, '13:00', 'Komputasi Bergerak'))//Komber
+    classes.push(new Kelas(client, '760318072719933481', 2, '13:00', 'Sistem Temu Kembali Informasi'))//STKI
+    classes.push(new Kelas(client, '760318108849930280', 2, '16:00', 'Visi Komputer'))//Viskom
+
+
+    //Rabu
+    classes.push(new Kelas(client, '760777448039645185', 3, '07:00', 'Biomedik'))//Biomedik
+    classes.push(new Kelas(client, '760776180236025856', 3, '10:00', 'Robotika'))//Robotika
+    classes.push(new Kelas(client, '760778052916609045', 3, '13:00', 'Teknologi Antar Jaringan'))//TAJ
+    classes.push(new Kelas(client, '760778085909528616', 3, '13:00', 'Sistem Enterprise'))//SE
+    classes.push(new Kelas(client, '760776302252392460', 3, '16:00', 'Teknik Pengembangan Game'))//TPG
+    classes.push(new Kelas(client, '760779088146464820', 3, '16:00', 'Sistem Informasi Geografis'))//SIG
+
     runSchedule()
 }
 

a99f117d1e595812b1a536088f837b1102e8a4cb
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2020-10-02 11:38:59 +0700

    Update: chink, euy. Send Link insted of File
    Faster

--- commands/bruh.js
+++ commands/bruh.js
@@ -7,7 +7,6 @@ module.exports = {
             'bruh3.jpg',
             'bruh4.jpg',
         ]
-        console.info(file_names)
         msg.channel.send('Bruh',
             {
                 file: `./assets/${file_names[Math.floor(Math.random() * file_names.length)]}`
--- commands/euy.js
+++ commands/euy.js
@@ -1,10 +1,7 @@
 module.exports = {
     name: 'euy',
     execute(msg,args){
-        msg.channel.send('ayeuna poe rebo euy',
-            {
-                file: './assets/rebo.mp4'
-            }
-        )
+        msg.channel.send('ayeuna poe rebo euy')
+        msg.channel.send('https://cdn.discordapp.com/attachments/760374936421400606/761446446548647946/rebo.mp4')        
     }
 };

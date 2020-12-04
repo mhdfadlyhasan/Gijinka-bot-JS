502a4082ac9ade02777b658b2f70ccd7d4a37c5a
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2020-12-04 22:37:16 +0700

    Add dynamic environmental variable for postgresql connection

--- utilities/db/connect.js
+++ utilities/db/connect.js
@@ -1,17 +1,15 @@
 const { Client } = require('pg')
 require('dotenv').config()
-
+console.log('connect')
 // use these to connect to heroku postgres db on local (might change, contact me if it did)
-var connectionString = 'postgres://ggjyddlnmfepns:37c2a5762e38648e7ce0eafacce3162f8c2b09139a8468b62babe0722d02ca9c@ec2-107-21-201-57.compute-1.amazonaws.com:5432/dkfsc40oli0ag'
-
-// var connectionString = process.env.DATABASE_URL
+var connectionString = process.env.DATABASE_URL
 
 const client = new Client({
     //   connectionString: process.env.DATABASE_URL,
     connectionString: connectionString,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
 })
 
 client.connect()

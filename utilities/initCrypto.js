f30ee466f7333e57e297b78a54133a5795ef1c5d
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-08-06 11:33:44 +0700

    Fix unworking loop

--- utilities/initCrypto.js
+++ utilities/initCrypto.js
@@ -1,15 +1,17 @@
 const axios = require("axios")
+const cron = require('node-cron')
 function startCryptoCron(client) {
     client.channels.get('873052497843978270').send('Harga crypto tiap menit').then((msg) => {
-        setTimeout( async () => {
+       cron.schedule('* * * * *', async function () {
             var str = await axios.get('https://indodax.com/api/price_increments')
-            console.log(`info tiap menit dikirim`)
-            msg.edit(str.data.increments.btc_idr)
-        },60000)
+                console.log(`info tiap menit dikirim`)
+                msg.edit(str.data.increments.btc_idr)
+        }, {
+            scheduled: true,
+            timezone: 'Asia/Jakarta'
+        })   
     })
 }
-
-
 module.exports = {
     startCryptoCron
 }

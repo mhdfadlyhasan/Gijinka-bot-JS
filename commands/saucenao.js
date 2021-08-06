bd3be9d05ecf0d6812d1bcb4ef30387cbca68a9b
mhd fadly hasan <mhdfadlyhasan16a1@gmail.com>
2021-08-06 22:16:17 +0700

    Update saucenao.js
    now user can upload image instead of sending link

--- commands/saucenao.js
+++ commands/saucenao.js
@@ -1,20 +1,41 @@
 const Sauce = require('saucenao')
 const _ = require('lodash')
-let sauce = new Sauce(process.env.API_KEY_SAUCENAO)
 module.exports = {
     name: 'saucenao',
     description: 'saucenao',
     usage: 'saucenao ',
     async execute(msg, args) {
-        var link = args[1]
-        let saus = (await sauce(link)).json
-        var hasil = ""
-        _.forEach(saus.results, function(element) {
-            if(parseFloat(element.header.similarity) > 70.0){
-                let link = element.data.ext_urls[0]
-                hasil += link + ` similarity ${element.header.similarity} | `
-            }
-        })
-        msg.reply(hasil);
+        if(msg.attachments.size>0){ //user may send multiple images
+            link = msg.attachments.url
+            msg.attachments.map(async function(val, key) {
+                let link = val.url
+                msg.reply(await getSauce(link))
+            })
+        }
+        else {
+            var link = args[1]
+            msg.reply(await getSauce(link))
+        }
     }
+}
+
+async function getSauce(link) {
+    let sauce = new Sauce(process.env.API_KEY_SAUCENAO)
+    let saus = (await sauce(link)).json
+    var hasil = ""
+    let found = false
+    _.forEach(saus.results, function(element) {
+        console.log(element.header.similarity)
+        if(parseFloat(element.header.similarity) > 30.0){
+            found = true
+            if(element.data.ext_urls != null) 
+            {
+                let link = element.data.ext_urls[0]
+                hasil += link + `\n similarity ${element.header.similarity} | `    
+            }
+            
+        }
+    })
+    if(!found) hasil = "not found"
+    return hasil
 }
